b3ff9d7e7c2c171a1d85e50d16f28219a58e6cc8
Liam_Coles <lcoles1988@gmail.com>
2023-08-16 20:56:54 +0100

    added mock places object to file so it can be used to any frontend testing

---
+++ frontend/mocks/mockPlaces.js
@@ -0,0 +1,435 @@
+const mockPlaceJSON =     
+{
+    "html_attributions": [],
+    "results": [
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.5691001,
+                    "lng": -0.2648901
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57044992989272,
+                        "lng": -0.2635402701072777
+                    },
+                    "southwest": {
+                        "lat": 52.56775027010728,
+                        "lng": -0.2662399298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/park-71.png",
+            "icon_background_color": "#4DB546",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/tree_pinlet",
+            "name": "The Boardwalks",
+            "opening_hours": {
+                "open_now": false
+            },
+            "photos": [
+                {
+                    "height": 2872,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/111717920048885351692\">A Google User</a>"
+                    ],
+                    "photo_reference": "AUacShh8hPrul0_Exne6VK45UxBN66j2KrL9-V55B4WMk3HwlE2z4xKV7PQuWllQ2EFH5HWNb5MJ-IOQEbR1JH-iiiui_QVkGexS0df9kFZ2qdxfhHJSmXfuMeOjc78ZhLhh97wuLce9GVUrdI9bn6-wUZr5xsc08unwTa86P0vzaGDWUCQh",
+                    "width": 4032
+                }
+            ],
+            "place_id": "ChIJb4soY6Hxd0gRAcNIhp5yXAA",
+            "plus_code": {
+                "compound_code": "HP9P+J2 Peterborough, United Kingdom",
+                "global_code": "9C4XHP9P+J2"
+            },
+            "rating": 4.5,
+            "reference": "ChIJb4soY6Hxd0gRAcNIhp5yXAA",
+            "scope": "GOOGLE",
+            "types": [
+                "park",
+                "tourist_attraction",
+                "point_of_interest",
+                "establishment"
+            ],
+            "user_ratings_total": 60,
+            "vicinity": "Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.5739421,
+                    "lng": -0.2454201
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57534547989273,
+                        "lng": -0.2445228701072777
+                    },
+                    "southwest": {
+                        "lat": 52.57264582010728,
+                        "lng": -0.2472225298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/shopping-71.png",
+            "icon_background_color": "#4B96F3",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/shopping_pinlet",
+            "name": "Menkind",
+            "opening_hours": {
+                "open_now": false
+            },
+            "photos": [
+                {
+                    "height": 1000,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/100616426512035784903\">Menkind Peterborough</a>"
+                    ],
+                    "photo_reference": "AUacShhFL5yRs9hKLdD6JdmNgOs2kW6Gq6vrqsN7DlvOWtOX5LyoPAVlUkrcRibB3Kqmv8OkCI4YqM5Xyjyu21-eITHJAe7TGs-YvwYpHI5Sh4AySOc5D1ZPTwVLs3rUlJf9Zd-uq4WhmmCsatGqYFrEZI-NIs8ytec6cUfXElSETe9rXNXj",
+                    "width": 1000
+                }
+            ],
+            "place_id": "ChIJt8C7rKrxd0gRYrps4l81Lig",
+            "plus_code": {
+                "compound_code": "HQF3+HR Peterborough, United Kingdom",
+                "global_code": "9C4XHQF3+HR"
+            },
+            "price_level": 2,
+            "rating": 4.2,
+            "reference": "ChIJt8C7rKrxd0gRYrps4l81Lig",
+            "scope": "GOOGLE",
+            "types": [
+                "point_of_interest",
+                "store",
+                "establishment"
+            ],
+            "user_ratings_total": 245,
+            "vicinity": "87 Queensgate, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.5730704,
+                    "lng": -0.2444367
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57419297989272,
+                        "lng": -0.2432321701072778
+                    },
+                    "southwest": {
+                        "lat": 52.57149332010727,
+                        "lng": -0.2459318298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png",
+            "icon_background_color": "#FF9E67",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet",
+            "name": "Turtle Bay Peterborough",
+            "opening_hours": {
+                "open_now": true
+            },
+            "photos": [
+                {
+                    "height": 5505,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/106673322884882345306\">A Google User</a>"
+                    ],
+                    "photo_reference": "AUacShjIUoPUR-P1UCOD0pGzMjzC-JF7wDCjF4fMPEtUkLsHz8rGMtOO3lElxgX4QAI26lScGU2lag2xViiPbo2z5Lus8H6sZOT3KHSL35qUe0ko-7_08ypkAnf0N3LBGJS4si1oyphYjy2b9g8QEWvmA3cR9v5K93Qd5X-_LObJlx1b_4LL",
+                    "width": 8256
+                }
+            ],
+            "place_id": "ChIJ_z9pkarxd0gRcVMt-6pI0V4",
+            "plus_code": {
+                "compound_code": "HQF4+66 Peterborough, United Kingdom",
+                "global_code": "9C4XHQF4+66"
+            },
+            "price_level": 2,
+            "rating": 4.6,
+            "reference": "ChIJ_z9pkarxd0gRcVMt-6pI0V4",
+            "scope": "GOOGLE",
+            "types": [
+                "bar",
+                "restaurant",
+                "point_of_interest",
+                "food",
+                "establishment"
+            ],
+            "user_ratings_total": 2937,
+            "vicinity": "46, Queensgate Shopping Centre, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.571818,
+                    "lng": -0.2458346
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57323097989272,
+                        "lng": -0.2444643701072778
+                    },
+                    "southwest": {
+                        "lat": 52.57053132010728,
+                        "lng": -0.2471640298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/museum-71.png",
+            "icon_background_color": "#13B5C7",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/museum_pinlet",
+            "name": "Peterborough Museum & Art Gallery",
+            "opening_hours": {
+                "open_now": false
+            },
+            "photos": [
+                {
+                    "height": 1067,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/116418426459231533879\">Peterborough Museum</a>"
+                    ],
+                    "photo_reference": "AUacShgNNLqX2n-Bcm3fVzk4zYxRz6S0Lj-7co5vkRAto60AnRd-BM7MWKvIJH8o0yYyRMeI7zh74hXxOieHA0NMTEed5LTODdggxDAkFZD5Uz-681UnKnI7_6uI8ChES2lHX_a6Wre4IStyp4ZusFwyLONjXMuGrZHXB2lyMJ3xZSH_s1pU",
+                    "width": 1600
+                }
+            ],
+            "place_id": "ChIJ4WJD-6rxd0gRebA7m7q8eDM",
+            "plus_code": {
+                "compound_code": "HQC3+PM Peterborough, United Kingdom",
+                "global_code": "9C4XHQC3+PM"
+            },
+            "rating": 4.5,
+            "reference": "ChIJ4WJD-6rxd0gRebA7m7q8eDM",
+            "scope": "GOOGLE",
+            "types": [
+                "tourist_attraction",
+                "museum",
+                "art_gallery",
+                "point_of_interest",
+                "establishment"
+            ],
+            "user_ratings_total": 892,
+            "vicinity": "51 Priestgate, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.5743136,
+                    "lng": -0.244698
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57595352989272,
+                        "lng": -0.2431964201072777
+                    },
+                    "southwest": {
+                        "lat": 52.57325387010728,
+                        "lng": -0.2458960798927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/shopping-71.png",
+            "icon_background_color": "#4B96F3",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/shopping_pinlet",
+            "name": "Flying Tiger Copenhagen",
+            "opening_hours": {
+                "open_now": false
+            },
+            "photos": [
+                {
+                    "height": 555,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/106678455606869872372\">Flying Tiger Copenhagen</a>"
+                    ],
+                    "photo_reference": "AUacShi_GamlQDRQlkNp6RT_ahsKiButpt9bdzYiOgYUVrLiy4ZyFU9NZi1JsdQxRKAc5zwREXMXD3LCZGxf6fmocj7m0tWMkiMhcJxvaKOesrb2WonS7QzEBfzchUlLodEQYieci3W01WvT7ZwTs9_V53Zyf1BtR22n4RrBYZqYXYEGxGuq",
+                    "width": 1290
+                }
+            ],
+            "place_id": "ChIJNWXQmKrxd0gRRinUKWVb36o",
+            "plus_code": {
+                "compound_code": "HQF4+P4 Peterborough, United Kingdom",
+                "global_code": "9C4XHQF4+P4"
+            },
+            "rating": 4.1,
+            "reference": "ChIJNWXQmKrxd0gRRinUKWVb36o",
+            "scope": "GOOGLE",
+            "types": [
+                "department_store",
+                "home_goods_store",
+                "point_of_interest",
+                "store",
+                "establishment"
+            ],
+            "user_ratings_total": 128,
+            "vicinity": "Queensgate Centre, Unit 72, Upper, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.5729984,
+                    "lng": -0.2464106
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57436377989273,
+                        "lng": -0.2450370701072777
+                    },
+                    "southwest": {
+                        "lat": 52.57166412010728,
+                        "lng": -0.2477367298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/bar-71.png",
+            "icon_background_color": "#FF9E67",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/bar_pinlet",
+            "name": "The Draper's Arms - JD Wetherspoon",
+            "opening_hours": {
+                "open_now": true
+            },
+            "photos": [
+                {
+                    "height": 2008,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/112289093622241173539\">A Google User</a>"
+                    ],
+                    "photo_reference": "AUacShhstk1tAmJh-4vJlITWImWPa7ZTv_I_IZ0aESS4gPdIjNncvQDpD_sHUJfoCQ7CY797ekm3dMQ57bOKoSeQ3SPPzDnqs78LAGAZFvxZtBn-cI-z4avx9hFpxTSGg6rGrN6abS264V6R9TOPXg3ZHp3HSmlVvrfOX_Yt7gNux_9NxYbr",
+                    "width": 2953
+                }
+            ],
+            "place_id": "ChIJf400larxd0gRXYsDm57kZIk",
+            "plus_code": {
+                "compound_code": "HQF3+6F Peterborough, United Kingdom",
+                "global_code": "9C4XHQF3+6F"
+            },
+            "price_level": 1,
+            "rating": 4.2,
+            "reference": "ChIJf400larxd0gRXYsDm57kZIk",
+            "scope": "GOOGLE",
+            "types": [
+                "bar",
+                "restaurant",
+                "point_of_interest",
+                "food",
+                "establishment"
+            ],
+            "user_ratings_total": 1882,
+            "vicinity": "29-31 Cowgate, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.56338659999999,
+                    "lng": -0.2610475
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.56473642989271,
+                        "lng": -0.2596976701072777
+                    },
+                    "southwest": {
+                        "lat": 52.56203677010727,
+                        "lng": -0.2623973298927221
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/shopping-71.png",
+            "icon_background_color": "#4B96F3",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/shoppingcart_pinlet",
+            "name": "Lidl",
+            "opening_hours": {
+                "open_now": true
+            },
+            "photos": [
+                {
+                    "height": 630,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/102636767695687256250\">shilna</a>"
+                    ],
+                    "photo_reference": "AUacShixXhZRxoucmCIhLl7SjGF_nkJKTEVhGLHzI3JYBCKCw_KW-fUbObw2X3uotO98wzbRxd9BJgTaOA2pbyraibk3OXiAF5KBieKU0b35IwuCgLAOx4dpeDzc24P3qjuIVUc0oX0uPzWFodITTlrZBlShE2gmcznWFigcGxqExFZqCFAs",
+                    "width": 1200
+                }
+            ],
+            "place_id": "ChIJW6PzU8fxd0gR6h7pDe6IstE",
+            "plus_code": {
+                "compound_code": "HP7Q+9H Peterborough, United Kingdom",
+                "global_code": "9C4XHP7Q+9H"
+            },
+            "rating": 4.4,
+            "reference": "ChIJW6PzU8fxd0gR6h7pDe6IstE",
+            "scope": "GOOGLE",
+            "types": [
+                "supermarket",
+                "grocery_or_supermarket",
+                "point_of_interest",
+                "food",
+                "store",
+                "establishment"
+            ],
+            "user_ratings_total": 714,
+            "vicinity": "Sugar Way, Peterborough"
+        },
+        {
+            "business_status": "OPERATIONAL",
+            "geometry": {
+                "location": {
+                    "lat": 52.572281,
+                    "lng": -0.2428909
+                },
+                "viewport": {
+                    "northeast": {
+                        "lat": 52.57363082989272,
+                        "lng": -0.2415410701072777
+                    },
+                    "southwest": {
+                        "lat": 52.57093117010728,
+                        "lng": -0.2442407298927222
+                    }
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png",
+            "icon_background_color": "#FF9E67",
+            "icon_mask_base_uri": "https://maps.gstatic.com/mapfiles/place_api/icons/v2/restaurant_pinlet",
+            "name": "Wildwood",
+            "opening_hours": {
+                "open_now": true
+            },
+            "photos": [
+                {
+                    "height": 2432,
+                    "html_attributions": [
+                        "<a href=\"https://maps.google.com/maps/contrib/114911959903234655236\">Wildwood</a>"
+                    ],
+                    "photo_reference": "AUacShg22dloKDo0br3mrNHrcUEHW6ZHEsnoOz7TBlEzFtJy2CsMkrFCA6-y_a1Ei5gc8yK8g2RwAwdPA24JSvs9vavPb-CUknOB1K9zGcZklxBfbTEDikkU6gELcCUgtuRogrA6zFIiWSORB28gWtEMzhL9qk1lagIUiGCeInGbOpI-VHMR",
+                    "width": 3863
+                }
+            ],
+            "place_id": "ChIJKZbOIlXwd0gRPLlaimNIe2Y",
+            "plus_code": {
+                "compound_code": "HQC4+WR Peterborough, United Kingdom",
+                "global_code": "9C4XHQC4+WR"
+            },
+            "price_level": 2,
+            "rating": 4,
+            "reference": "ChIJKZbOIlXwd0gRPLlaimNIe2Y",
+            "scope": "GOOGLE",
+            "types": [
+                "restaurant",
+                "point_of_interest",
+                "food",
+                "establishment"
+            ],
+            "user_ratings_total": 979,
+            "vicinity": "6, 6A Cathedral Square, Peterborough"
+        }
+    ],
+    "status": "OK"
+}
+    
+
+module.exports = mockPlaceJSON;
