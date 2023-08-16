29718fa1a688a41995e81d6fc3db976927e6d66b
Fahim Islam <fahim.islam@outlook.com>
2023-08-16 15:35:26 +0100

    Find Places API working

--- api/controllers/attractions.js
+++ api/controllers/attractions.js
@@ -1,22 +1,54 @@
+const fetch = require("node-fetch");
 const API_KEY = 'AIzaSyBTiJKEAP4neyFSf3XgmgI4qrlDN3DPXow';
-const location = "Makers Academy"
+
 
 const AttractionsController = {
+  
   GetAttractions: async (req, res) => {
+
     try {
-      fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=formatted_address%2Cname%2Cgeometry&input=${location}&inputtype=textquery&key=${API_KEY}`)
-        .then((response) => response.json())
-        .then((data) => {
-          return res.status(201).json(data.candidates[0])
-      });
+      let locationsCoordinates = []
+      const locations = Array.from(req.body)
+
+      for (const location of locations) {
+        const getCoordinate = await getLocationCoordinates(location);
+        const label = `${location}`;
+        locationsCoordinates.push({[label]: getCoordinate})
+      }
+
+      res.status(201).json(locationsCoordinates);
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Error fetching location data' });
+      console.error('Error fetching coordinates:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
+  },
+};
+
+async function getLocationCoordinates(location) {
+  try {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${location}&key=${API_KEY}`);
+    const data = await response.json();
+
+    const results = data.results;
+    if (results.length > 0) {
+      const coordinates = results[0].geometry.location;
+      return { latitude: coordinates.lat, longitude: coordinates.lng };
+    } else {
+      throw new Error('Location not found.');
+    }
+  } catch (error) {
+    throw new Error('Error fetching location coordinates.');
   }
 }
 
+
+
+
+
+
+
 module.exports = AttractionsController;
+
 
 
 
--- api/spec/controllers/attractions.spec.js
+++ api/spec/controllers/attractions.spec.js
@@ -4,10 +4,43 @@ require('../mongodb_helper');
 
 
 describe('POST, google search place api', () => {
-  test('the response code is 201 and contains coordinates', async () => {
+  test('the response code is 201', async () => {
     let response = await request(app)
       .post('/attractions')
-    console.log(response)
     expect(response.statusCode).toBe(201);
+  });
+
+  test('When given two string locations it should return an array of two object with coordinates', async () => {
+    let response = await request(app)
+      .post('/attractions')
+      .send(["Makers Academy", "Tower Bridge"])
+    expect(response.body).toEqual([
+      {
+        'Makers Academy': { latitude: 51.5234156, longitude: -0.08354919999999999 }
+      },
+      {
+        'Tower Bridge': { latitude: 51.5054564, longitude: -0.07535649999999999 }
+      }
+    ]);
+  });
+
+  test('When given four string locations it should return an array of four objects with coordinates', async () => {
+    let response = await request(app)
+      .post('/attractions')
+      .send(["Makers Academy", "London Eye", "Le Wagon", "Tower Bridge"])
+    expect(response.body).toEqual([
+      {
+        'Makers Academy': { latitude: 51.5234156, longitude: -0.08354919999999999 }
+      },
+      { 
+        'London Eye': { latitude: 51.5031864, longitude: -0.1195192 } 
+      },
+      {
+        'Le Wagon': { latitude: 51.53280849999999, longitude: -0.0769292 }
+      },
+      {
+        'Tower Bridge': { latitude: 51.5054564, longitude: -0.07535649999999999 }
+      }
+    ])
   });
 });
