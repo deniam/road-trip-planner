3d8d7197d82e2edf904a1f521114aa6c1f9fd62b
Liam_Coles <lcoles1988@gmail.com>
2023-08-17 16:17:04 +0100

    added new attractionList component with tests

---
+++ frontend/src/components/attractions/AttractionList.js
@@ -0,0 +1,51 @@
+import React, { useState, useLayoutEffect } from 'react';
+import Attraction from './Attraction';
+
+const AttractionList = ({ navigate, attractions, disableClicks, disableNextButton, submitAttractions} ) => {
+    const [attractionsWithId, setAttractionsWithId] = useState([]);
+
+    useLayoutEffect(() =>{
+        let newAttractionsArray = []
+        
+        for (let i = 0; i < attractions.length; i++){
+            newAttractionsArray.push({id:i, attraction:attractions[i], selected: false})
+        } 
+        setAttractionsWithId(newAttractionsArray)
+    }, [])
+
+    const attractionClicked = (id, selected) =>{
+        const updatedattractionsWithId = attractionsWithId.map(attractionWithId =>
+            attractionWithId.id === id ? { ...attractionWithId, selected } : attractionWithId
+        );
+        setAttractionsWithId(updatedattractionsWithId);
+    }
+
+    const handleNextButtonClick = (event) => {
+        event.preventDefault();
+        let finalAttractionsList = []
+        for (let i = 0; i < attractionsWithId.length; i++){
+            if (attractionsWithId[i].selected){
+                finalAttractionsList.push(attractionsWithId[i].attraction)
+            }
+        } 
+        submitAttractions(finalAttractionsList);
+        navigate('/planner/final');
+    }
+
+    return (
+        <div>
+            {attractionsWithId.map(attractionWithId => (
+                <div key={attractionWithId.id}>
+                    <Attraction id={attractionWithId.id} attraction={attractionWithId.attraction} disableClick={disableClicks} attractionClicked={attractionClicked} />
+                </div>
+            ))}
+            {disableNextButton ? null:<button onClick={handleNextButtonClick} className="nextButton"> Next </button>}
+        </div>
+    )
+
+}
+
+
+
+
+export default AttractionList;
