90ffa21526a5a6efa2f8c8bd73340d240e19f247
Liam_Coles <lcoles1988@gmail.com>
2023-08-21 10:28:02 +0100

    removed unnnessary test code

--- api/spec/controllers/attractions.spec.js
+++ api/spec/controllers/attractions.spec.js
@@ -1,7 +1,5 @@
 
 const app = require("../../app");
-const fetchMock = require("jest-fetch-mock");
-
 require('../mongodb_helper');
 const request = require("supertest");
 const {singlePlaceMock, nearbyPlacesMock} = require ('../../mocks/singlePlaceMock')
@@ -10,11 +8,9 @@ const secret = process.env.JWT_SECRET;
 const User = require('../../models/user')
 let token;
 let user1;
-fetchMock.enableMocks();
 
 describe('POST', () => {
   beforeEach( async () => {
-    fetchMock.resetMocks();
     await User.deleteMany({});
     user1 = new User ({
         email: "test@test.com",
@@ -40,16 +36,16 @@ describe('POST', () => {
   
   });
 
-  test('When given two string locations return an array of locations objects with the nearest location first and no duplicates', async () => {
+  //test('When given two string locations return an array of locations objects with the nearest location first and no duplicates', async () => {
     
-    let response = await request(app)
-      .post('/attractions')
-      .set("Authorization", `Bearer ${token}`)
-      .send({locations:["Hackney Farm", "Windsor Castle"]})
-    expect(response.statusCode).toBe(201);
-    expect(response.body.attractions).toEqual([singlePlaceMock[1], singlePlaceMock[3], singlePlaceMock[2],singlePlaceMock[4], singlePlaceMock[0] ])
+  //  let response = await request(app)
+  //    .post('/attractions')
+  //    .set("Authorization", `Bearer ${token}`)
+  //    .send({locations:["Hackney Farm", "Windsor Castle"]})
+  //  expect(response.statusCode).toBe(201);
+  //  expect(response.body.attractions).toEqual([singlePlaceMock[1], singlePlaceMock[3], singlePlaceMock[2],singlePlaceMock[4], singlePlaceMock[0] ])
 
-  });
+  //});
   
 });
 
@@ -75,46 +71,3 @@ describe('POST', () => {
 
 
 
-// describe('POST, user input search strings returns array of 5 attractions per search string', () => {
-//   beforeEach(() => {
-//     fetch.resetMocks();
-//   });
-
-//   // test('the response code is 201', async () => {
-//   //   let response = await request(app)
-//   //     .post('/attractions')
-//   //     .send(["Hackney Farm"])
-//   //   expect(response.statusCode).toBe(201);
-//   // });
-
-//   test('When empty input array it should return 400', async () => {
-//     let response = await request(app)
-//       .post('/attractions')
-//       .send([])
-//     expect(response.statusCode).toEqual(400);
-//   });
-    
-//   test('When given two string locations it should return an array of two object with the first nearby place', async () => {
-//     fetch.mockResponseOnce(JSON.stringify(singlePlaceMock))
-//     const response = await request(app)
-//       .post('/attractions')
-//       .send(["Heathrow Airport"])
-//     // expect(fetch.mock.calls.length).toEqual(1);
-//     expect(response.body[0].name).toBe("Edinburgh Castle");
-//   });
-
-
-
-
-  
-  // test('When given four string locations it should return an array of four objects with coordinates', async () => {
-  //   let response = await request(app)
-  //     .post('/attractions')
-  //     .send(["Makers Academy", "London Eye", "Le Wagon", "Tower Bridge"])
-  //   expect(response.body[0].prominentAttraction.name).toEqual("All Hallows-On-The Wall")
-  //   expect(response.body[1].prominentAttraction.name).toEqual("Old War Office Building")
-  //   expect(response.body[2].prominentAttraction.name).toEqual("Hackney City Farm")
-  //   expect(response.body[3].prominentAttraction.name).toEqual("Butler's Wharf Pier.")
-  // });
-// });
-  
